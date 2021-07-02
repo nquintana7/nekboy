d1462f691939fa49a030a6e7a34f4232c75b6205
Nicolas <quintananicolasc@gmail.com>
2021-07-02 19:43:54 +0200

    Fix

--- src/MemoryLocation.java
+++
@@ -1,6 +0,0 @@
-package src;
-
-public interface MemoryLocation {
-    public int getByte();
-    public void writeByte(int addr, int value);
-}
--- src/module-info.java
+++
@@ -1,3 +0,0 @@
-module Emulator {
-    requires java.desktop;
-}
